73404307a3175de0f49a76c3b0429551b398a177
Qyriad <qyriad@qyriad.me>
2025-08-13 12:57:02 +0200

    maint: minor refactor of Nix packaging

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   pkgs ? import <nixpkgs> { },
   python3Packages ? pkgs.python3Packages,
   qpkgs ? let
-    src = fetchTarball "https://github.com/Qyriad/nur-packages/archive/main.tar.gz";
+    src = fetchTree <| builtins.parseFlakeRef "github:Qyriad/nur-packages";
   in import src { inherit pkgs; },
 }: let
   inherit (pkgs) lib;
@@ -10,7 +10,8 @@
   niz = python3Packages.callPackage ./package.nix { };
 
   overrideCall = scope: scope.callPackage niz.override { };
-  notDisabled = p: !((p.meta or { }).disabled or false);
+  # nb: the parentheses here are unnecessary but it looks fucking weird.
+  notDisabled = p: !(p.meta.disabled or false);
 
   byPythonVersion = qpkgs.pythonScopes
   |> lib.mapAttrs (lib.const overrideCall)
